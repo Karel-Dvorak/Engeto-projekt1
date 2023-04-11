dd9e01eece0da0b6dcdfda363ce777b03d934c38
Karel <dvorak.karl@seznam.cz>
2023-04-11 22:59:52 +0200

    Téměř hotovo, jenom doladit vzhled

--- script_textový_editor.py
+++ script_textový_editor.py
@@ -39,7 +39,8 @@ uzivatele = {
     'mike':'password123',
     'liz':'pass123'
 }
-oddelovac = '-' * 40
+oddelovac = '-' * 60
+oddelovac_kapitola = '=' * 60
 # Vyžádá si od uživatele přihlašovací jméno a heslo
 
 user_name = input('User name:')
@@ -50,7 +51,7 @@ password = input('password:')
 if uzivatele.get(user_name) == password:
     print(oddelovac)
     print('Welcome to the app, bob \n We have 3 texts to be analyzed.')
-    print(oddelovac)
+    print(oddelovac_kapitola)
 else:
     print('unregistered user, terminating the program..')
     quit()
@@ -58,30 +59,69 @@ else:
 # Program nechá uživatele vybrat mezi třemi texty, uloženými v proměnné TEXTS:
 print(f"Text č.1 {TEXTS[0]}", oddelovac, sep='\n')
 print(f"Text č.2\n {TEXTS[1]}", oddelovac, sep='\n')
-print(f"Text č.3\n {TEXTS[2]}", oddelovac, sep='\n')
+print(f"Text č.3\n {TEXTS[2]}", oddelovac_kapitola, sep='\n')
 
 # Pokud uživatel vybere takové číslo textu, které není v zadání, program jej upozorní a skončí,
 vyber_textu = input('Zadej číslo textu:')
 
-#if vyber_textu not in int(TEXTS[3]):
-   # print('Číslo není ve výběru, ukončuji program!')
-    #quit()
+if vyber_textu.isnumeric() and 1 <= int(vyber_textu) <= 3:
+    print(f"Vybraný text: {TEXTS[int(vyber_textu) -1]}", oddelovac_kapitola, sep='\n')
 #pokud uživatel zadá jiný vstup než číslo, program jej rovněž upozorní a skončí.
 
-# Pro vybraný text spočítá následující statistiky:
+elif vyber_textu.isalpha():
+    print('Toto není číslo, ukončuji')
+    quit()
+else:
+    print('Číslo není ve výběru, ukončuji program!')
+    quit()
 
-# počet slov,
-# počet slov začínajících velkým písmenem,
-# počet slov psaných velkými písmeny,
+# Pro vybraný text spočítá následující statistiky:
+# počet slov
+
+cisty_text = []
+for slovo in TEXTS[int(vyber_textu) - 1].split():
+    ciste_slovo = slovo.strip(',.!?-')
+    cisty_text.append(ciste_slovo.lower())
+#počet slov začínajících velkým písmenem,
 # počet slov psaných malými písmeny,
 # počet čísel (ne cifer),
+# unikátní (typu 30N )
 # sumu všech čísel (ne cifer) v textu.
+slova_velkymi = []
+slova_malymi = []
+slova_cisla = []
+slova_velka = []
+unikatni = []
 
+for slovo in TEXTS[int(vyber_textu) - 1].split():
+    slovo_v = slovo.strip(',.!?-')
+    if slovo.istitle() and slovo.isupper():
+        unikatni.append(slovo_v)
+    elif slovo.isdigit():
+        slova_cisla.append(int(slovo_v))
+    elif slovo.istitle() or slovo.isupper():
+        slova_velkymi.append(slovo_v)
+    else:
+        slova_malymi.append(slovo_v)
+for velka_p in slova_velkymi:
+    if velka_p.isupper():
+        slova_velka.append(velka_p)
+
+print(f"""Počet slov: {len(cisty_text)}
+Počet slov začínající velkým písmenem: {len(slova_velkymi)}
+Počet slov s velkými písmeny {len(slova_velka)}
+Počet slov začínající malým písmenem: {len(slova_malymi)}
+Počet čísel: {len(slova_cisla)}
+Suma všech čísel: {sum(slova_cisla[:])}""")
 # Program zobrazí jednoduchý sloupcový graf, který bude reprezentovat 
 # četnost různých délek slov v textu. 
-# Například takto:
-# 7| * 1
-# 8| *********** 11
-# 9| *************** 15
-# 10| ********* 9
-# 11| ********** 10
+
+print(oddelovac, f"LEN|    OCCURENCES   |NR.", oddelovac, sep = '\n')
+delka_slov = {}
+for delka in cisty_text:
+    if len(delka) not in delka_slov:
+        delka_slov[len(delka)] = 1
+    else:
+        delka_slov[len(delka)] = delka_slov[len(delka)] + 1
+for key, value in sorted(delka_slov.items()):
+    print(f"{key: ^3}|{value * '*': <17}|{value}")
